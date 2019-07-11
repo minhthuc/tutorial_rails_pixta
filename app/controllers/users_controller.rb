3d5c438caf4491f11cc93962bec072e360896bcc
minhthuc229 <minhthuc229@gmail.com>
2019-07-11 14:21:07 +0700

    Add SSL in production and fix local var in parital view

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,6 @@
 class UsersController < ApplicationController
   def show
-    @user = User.find_by id: params[:id]
+    @user = User.find params[:id]
   end
 
   def new
@@ -9,16 +9,18 @@ class UsersController < ApplicationController
 
   def create
     @user = User.new(user_params)
+
     if @user.save
       flash[:success] = "Welcome to the Sample App!"
       redirect_to @user
     else
-      render "new"
+      render :new
     end
   end
 
   private
-    def user_params
-      params.require(:user).permit(:name, :email, :password, :password_confirmation)
-    end
+
+  def user_params
+    params.require(:user).permit(:name, :email, :password, :password_confirmation)
+  end
 end
--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -8,30 +8,30 @@ RSpec.feature do
     describe "Home page" do
       before { visit root_path }
 
-      it { should have_content('Sample App') }
-      it { should have_title(full_title('')) }
-      it { should_not have_title('|Home') }
+      it { is_expected.to have_content('Sample App') }
+      it { is_expected.to have_title(full_title('')) }
+      it { is_expected.to_not have_title('|Home') }
     end
 
     describe "Help page" do
       before { visit help_path }
 
-      it { should have_content('Help') }
-      it { should have_title(full_title('Help')) }
+      it { is_expected.to have_content('Help') }
+      it { is_expected.to have_title(full_title('Help')) }
     end
 
     describe "About page" do
       before { visit about_path }
 
-      it { should have_content('About') }
-      it { should have_title(full_title('About Us')) }
+      it { is_expected.to have_content('About') }
+      it { is_expected.to have_title(full_title('About Us')) }
     end
 
     describe "Contact page" do
       before { visit contact_path }
 
-      it { should have_content('Contact') }
-      it { should have_title(full_title('Contact')) }
+      it { is_expected.to have_content('Contact') }
+      it { is_expected.to have_title(full_title('Contact')) }
     end
   end
 end
