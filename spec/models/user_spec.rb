fd82d67c594d0910e8ae005836d390b8b4198ecd
minhthuc229 <minhthuc229@gmail.com>
2019-07-17 10:40:02 +0700

    Add user following

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -18,6 +18,11 @@ describe User do
   it { is_expected.to respond_to(:authenticate) }
   it { is_expected.to respond_to(:microposts) }
   it { is_expected.to respond_to(:feed) }
+  it { is_expected.to respond_to(:following?) }
+  it { is_expected.to respond_to(:followed_users) }
+  it { is_expected.to respond_to(:follow!) }
+  it { is_expected.to respond_to(:reverse_relationships) }
+  it { is_expected.to respond_to(:followers) }
 
   context "remember token" do
     before { user.save }
@@ -93,6 +98,10 @@ describe User do
   it { is_expected.to respond_to(:authenticate) }
   it { is_expected.to respond_to(:admin) }
   it { is_expected.to respond_to(:microposts) }
+  it { is_expected.to respond_to(:relationships) }
+  it { is_expected.to respond_to(:followed_users) }
+  it { is_expected.to respond_to(:follow!) }
+  it { is_expected.to respond_to(:unfollow!) }
 
   it { is_expected.to be_valid }
   it { is_expected.not_to be_admin }
@@ -136,6 +145,27 @@ describe User do
       its(:feed) { is_expected.to include(newer_micropost) }
       its(:feed) { is_expected.to include(older_micropost) }
       its(:feed) { is_expected.not_to include(unfollowed_post) }
+    end
+  end
+
+  describe "following" do
+    let (:other_user) { create(:user) }
+
+    before do
+      user.save
+      user.follow!(other_user)
+    end
+    it { is_expected.to be_following(other_user) }
+    its(:followed_users) { is_expected.to include(other_user) }
+    describe "and unfollowing" do
+      before { user.unfollow!(other_user) }
+
+      it { is_expected.to be_following(other_user) }
+      its(:followed_users) { is_expected.to_not include(other_user) }
+      describe "followed user" do
+        subject { other_user }
+        its(:followers) { is_expected.to include(user) }
+      end
     end
   end
 end
--- spec/requests/user_pages_spec.rb
+++ spec/requests/user_pages_spec.rb
@@ -20,7 +20,58 @@ RSpec.feature do
 
       it { is_expected.to have_content(user.name) }
       it { is_expected.to have_title(user.name) }
+
+      describe "follow/unfollow buttons" do
+        let(:other_user) { FactoryGirl.create(:user) }
+        before { sign_in user }
+
+        describe "following a user" do
+          before { visit user_path(other_user) }
+
+          it "should increment the followed user count" do
+            expect do
+              click_button "Follow"
+            end.to change(user.followed_users, :count).by(1)
+          end
+
+          it "should increment the other user's followers count" do
+            expect do
+              click_button "Follow"
+            end.to change(other_user.followers, :count).by(1)
+          end
+
+          describe "toggling the button" do
+            before { click_button "Follow" }
+            it { is_expected.to have_xpath("//input[@value='Unfollow']") }
+          end
+        end
+
+        describe "unfollowing a user" do
+          before do
+            user.follow!(other_user)
+            visit user_path(other_user)
+          end
+
+          it "should decrement the followed user count" do
+            expect do
+              click_button "Unfollow"
+            end.to change(user.followed_users, :count).by(-1)
+          end
+
+          it "should decrement the other user's followers count" do
+            expect do
+              click_button "Unfollow"
+            end.to change(other_user.followers, :count).by(-1)
+          end
+
+          describe "toggling the button" do
+            before { click_button "Unfollow" }
+            it { should have_xpath("//input[@value='Follow']") }
+          end
+        end
+      end
     end
+
     context "signup page" do
       before { visit signup_path }
 
@@ -115,7 +166,7 @@ RSpec.feature do
       describe "pagination" do
 
         before(:all) { 30.times { FactoryGirl.create(:user) } }
-        after(:all)  { User.delete_all }
+        after(:all) { User.delete_all }
 
         it { should have_selector("div.pagination") }
 
@@ -128,7 +179,7 @@ RSpec.feature do
 
       describe "delete links" do
 
-        it { should_not have_link("delete") }
+        it { is_expected.not_to have_link("delete") }
 
         describe "as an admin user" do
           let(:admin) { FactoryGirl.create(:admin) }
@@ -143,10 +194,70 @@ RSpec.feature do
               click_link("delete", match: :first)
             end.to change(User, :count).by(-1)
           end
-          it { should_not have_link("delete", href: user_path(admin)) }
+          it { is_expected.not_to have_link("delete", href: user_path(admin)) }
         end
       end
 
+    end
+
+    describe "following/followers" do
+      let(:user) { FactoryGirl.create(:user) }
+      let(:other_user) { FactoryGirl.create(:user) }
+      before { user.follow!(other_user) }
+
+      describe "followed users" do
+        before do
+          sign_in user
+          visit following_user_path(user)
+        end
+
+        it { is_expected.to have_title(full_title("Following")) }
+        it { is_expected.to have_selector("h3", text: "Following") }
+        it { is_expected.to have_link(other_user.name, href: user_path(other_user)) }
+      end
+
+      describe "followers" do
+        before do
+          sign_in other_user
+          visit followers_user_path(other_user)
+        end
+
+        it { is_expected.to have_title(full_title("Followers")) }
+        it { is_expected.to have_selector("h3", text: "Followers") }
+        it { is_expected.to have_link(user.name, href: user_path(user)) }
+      end
+    end
+
+    describe "micropost associations" do
+      let(:user) { FactoryGirl.create(:user) }
+      before { user.save }
+      subject { user }
+      let!(:older_micropost) do
+        FactoryGirl.create(:micropost, user: user, created_at: 1.day.ago)
+      end
+      let!(:newer_micropost) do
+        FactoryGirl.create(:micropost, user: user, created_at: 1.hour.ago)
+      end
+      describe "status" do
+        let(:unfollowed_post) do
+          FactoryGirl.create(:micropost, user: FactoryGirl.create(:user))
+        end
+        let(:followed_user) { FactoryGirl.create(:user) }
+
+        before do
+          user.follow!(followed_user)
+          3.times { followed_user.microposts.create!(content: "Lorem ipsum") }
+        end
+
+        its(:feed) { is_expected.to include(newer_micropost) }
+        its(:feed) { is_expected.to include(older_micropost) }
+        its(:feed) { is_expected.not_to include(unfollowed_post) }
+        its(:feed) do
+          followed_user.microposts.each do |micropost|
+            should include(micropost)
+          end
+        end
+      end
     end
   end
 end
